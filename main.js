c57bb4db464ab873246b67b205a1868221353fdc
sprihozhiy <sprihozhiy@gmail.com>
2020-07-09 22:22:10 -0600

    deleted contact nav link and script for scrolling

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const pageTop = document.querySelector("header");
 const pageProjects = document.getElementById("projects");
-const pageContact = document.getElementById("contact-form");
 
 const toProjects = document.getElementById("toProjects");
-const toContact = document.getElementById("toContact");
-
-//Need to refactor code below, combining two similar functions to one
 
 function moveToProjects(e) {
   e.preventDefault();
@@ -13,10 +9,3 @@ function moveToProjects(e) {
 }
 
 toProjects.addEventListener("click", moveToProjects);
-
-function moveToContact(e) {
-  e.preventDefault();
-  pageContact.scrollIntoView({ block: "start", behavior: "smooth" });
-}
-
-toContact.addEventListener("click", moveToContact);
